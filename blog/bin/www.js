f709872527b8a6828a8449aa864b0efa4916f925
CCYO <tume20938@gmail.com>
2020-11-04 11:31:54 +0800

    更改監聽code

--- blog/bin/www.js
+++ blog/bin/www.js
@@ -5,6 +5,6 @@ const serverHandle = require('../app')
 
 const server = http.createServer(serverHandle)
 
-server.listen(PORT, ()=>{
+server.listen(PORT, "0.0.0.0", ()=>{
     console.log(`NodeJS在後台 listen ${PORT} OK`)
 })
